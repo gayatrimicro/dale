b5ca36fa8bcdc813329378dd426ada637f61ba09
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-04-16 16:42:50 +0530

    new mail structure

--- contact-us/requestaapp2.php
+++ contact-us/requestaapp2.php
@@ -11,7 +11,7 @@
 	 
 	 // $location = $_POST["location"];
 
-	 $subject ="Claim Request " . $fname;
+	 $subject ="CallBack Request by " . $fname;
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 	$headers .= 'From: Dale R. Rose <' . $emailid .">" ."\r\n";
